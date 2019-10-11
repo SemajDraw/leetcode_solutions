aa42bfc4364109f17ae1ff3d07772a2c4f6bab4c
SemajDraw <32390574+SemajDraw@users.noreply.github.com>
2019-10-11 18:30:47 +0100

    Reverse Integer, Palindrome Number, Roman to Integer

---
+++ algorithms/reverse_integer.py
@@ -0,0 +1,18 @@
+
+
+def reverse(x: int) -> int:
+    str_num = str(x)
+    minus = False
+    if str_num[0] == '-':
+        minus = True
+        str_num = str_num[1:]
+    rev_num = str_num[::-1]
+    if minus:
+        rev_num = '-' + rev_num
+    rev_num = int(rev_num)
+    return rev_num if abs(rev_num) < (1 << 31) - 1 else 0
+
+
+if __name__ == "__main__":
+
+    print(reverse(-345))
