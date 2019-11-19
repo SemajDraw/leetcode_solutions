0d961d826c4698a0c5404b15bc74e6b06bbb4f0d
James Ward <c12404762@mydit.ie>
2019-11-19 16:40:23 +0000

    Valid sudoku completed, medium difficulty, tricky

--- algorithms/valid_sudoku.py
+++ algorithms/valid_sudoku.py
@@ -1,4 +1,4 @@
-
+""" Medium Difficulty"""
 
 def one2nine():
     return {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0}
@@ -32,29 +32,42 @@ def is_valid_sudoku(board) -> bool:
         column += 1
 
     # Check 3 x 3 boxes
-    boxes = 1
-    row = col = 0
-    row_boxs = 3
-    col_boxs = 3
-    while boxes != 3:
-        temp = one2nine()
-        while row != row_boxs:
-            while col != col_boxs:
-                val = board[row][col]
-                if val in temp.keys():
-                    temp[val] += 1
-                col += 1
-            row += 1
-            col = 0
-        row = 0
-        for key, val in temp.items():
-            if val > 1:
-                return False
+    box_rows = 3
+    box_columns = 3
+    rows = cols = 0
+    row_count = 3
+    col_count = 3
+    while box_rows != 0:
+        while box_columns != 0:
+            temp = one2nine()
+            while rows < row_count:
+                while cols < col_count or cols == 8:
+                    box_val = board[rows][cols]
+                    if box_val in temp.keys():
+                        temp[box_val] += 1
+                    cols += 1
+                cols -= 3
+                rows += 1
+            for key, val in temp.items():
+                if val > 1:
+                    return False
+            if col_count == 9:
+                col_count = 0
+            col_count += 3
+            cols += 3
+            rows -= 3
+            box_columns -= 1
+        box_columns = 3
+        box_rows -= 1
+        row_count += 3
+        cols = 0
+        rows = row_count - 3
 
+    return True
 
 
 if __name__ == '__main__':
-    is_valid_sudoku([
+    print(is_valid_sudoku([
         ["5","3",".",".","7",".",".",".","."],
         ["6",".",".","1","9","5",".",".","."],
         [".","9","8",".",".",".",".","6","."],
@@ -64,4 +77,4 @@ if __name__ == '__main__':
         [".","6",".",".",".",".","2","8","."],
         [".",".",".","4","1","9",".",".","5"],
         [".",".",".",".","8",".",".","7","9"]
-        ])
+        ]))
